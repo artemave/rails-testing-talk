fbe0bd24df1acee91aac9e16e9fb760ac8adb28c
artemave <artemave@gmail.com>
2019-09-12 14:06:05 +0200

    Headless Chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,10 @@
 
 require 'test_helper'
 
+Capybara.register_driver :headless_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, args: %w[headless])
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: ENV['GUI'].present? ? :chrome : :headless_chrome, screen_size: [1400, 1400]
 end
